b2276d7fdd7eab7d348fe9b064ce7f5ec7490642
skennedy93 <sean47900@gmail.com>
2020-11-18 13:40:19 -0500

    installed material UI, attempting to create cards for recipes

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
 import Title from './Title';
 import Image from './Image';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+const styles = theme => ({
+	container: {
+	  padding: 20,
+	  marginTop: 70,
+	},
+	card: {
+	  marginTop: 25,
+	  position: 'relative',
+	  overflow: 'visible',
+	  minWidth: '40%',
+	  minHeight: 350,
+	  zIndex: 0,
+	},
+	paper: {
+	  margin: 'auto',
+	  marginTop: 50,
+	},
+
+  });
 
 export default class Recipe extends Component {
 	render(){
+		const { classes } = this.props;
 		return(
           <div>
-         	{this.props.meals.map((item, index) => {
+		      <Grid container spacing={2}>
+			  <Grid item xs={12}>
+				<Grid container justify="center" >
+				{this.props.meals.map((item, index) => {
          		return ( 
-         			<div 
+         			<div
          				key={index} 
          				href={item.strSource}>
          				<Image source={item.strMealThumb} text={item.strMeal} />
@@ -16,7 +42,12 @@ export default class Recipe extends Component {
          			</div> 
          		)
          	})}
+
+				</Grid>
+			  </Grid>
+			</Grid>
           </div>
+
 		)
 	}
 }
